60c44bd4d647892eca23655d08aeeed20e6c69d8
Przemyslaw Czuj <przczuj@gmail.com>
2014-11-08 21:16:34 +0100

    Added ScheduledExecutor with placeholder task to applet initialization

--- src/main/java/TestApplet.java
+++ src/main/java/TestApplet.java
@@ -1,9 +1,23 @@
 import javax.swing.*;
 import java.applet.*;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class TestApplet extends Applet {
 
+    private final static int TICK_RATE_MILLISEC = 50;
+
     public void init() {
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                // placeholder
+            }
+        }, /*first execution delay*/0, TICK_RATE_MILLISEC, TimeUnit.MILLISECONDS);
+
         try {
             JOptionPane.showMessageDialog(this, "Hello World!", "vpl-pg ;>", JOptionPane.INFORMATION_MESSAGE);
         }
