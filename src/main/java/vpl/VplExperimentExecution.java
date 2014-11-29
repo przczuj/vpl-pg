41964820fe2d24d07a70d2827a7d34438f63758a
Przemyslaw Czuj <przczuj@gmail.com>
2014-11-29 16:44:36 +0100

    updated model concept

--- src/main/java/vpl/VplExperimentExecution.java
+++ src/main/java/vpl/VplExperimentExecution.java
@@ -12,5 +12,5 @@ import java.util.List;
  */
 public class VplExperimentExecution {
     private VplExperiment experiment;
-    private List<VplExperimentSnapshot> snapshots
+    private List<VplExperimentSnapshot> snapshots;
 }
