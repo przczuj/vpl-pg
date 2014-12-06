a675262e750777abbd0d444249995140c8b21fa3
Krzysztof Barszcz <keesio@wp.pl>
2014-12-06 23:39:39 +0100

    From Krzysiek P. Minor change in Force class in getForce(< changed to <=)

--- src/main/java/vpl/physics/Force.java
+++ src/main/java/vpl/physics/Force.java
@@ -1,52 +1,55 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package vpl.physics;
+	
 
-import vpl.math.Triple;
-import lombok.Getter;
-import lombok.Setter;
-
-/**
- *
- * @author kppx
- */
-public class Force {
-
-    @Setter
-    private Triple forceValue;
-    @Getter
-    @Setter
-    private Triple forceLocation;
-    @Getter
-    @Setter
-    private double timeToLive;
-    @Getter
-    @Setter
-    private boolean forever;
-
-    public Force() {
-        forceValue = new Triple();
-        forceLocation = new Triple();
-        timeToLive = 1;
-        forever = true;
+    /*
+     * To change this template, choose Tools | Templates
+     * and open the template in the editor.
+     */
+    package vpl.physics;
+     
+    import vpl.math.Triple;
+    import lombok.Getter;
+    import lombok.Setter;
+     
+    /**
+     *
+     * @author kppx
+     */
+    public class Force {
+     
+        @Setter
+        private Triple forceValue;
+        @Getter
+        @Setter
+        private Triple forceLocation;
+        @Getter
+        @Setter
+        private double timeToLive;
+        @Getter
+        @Setter
+        private boolean forever;
+     
+        public Force() {
+            forceValue = new Triple();
+            forceLocation = new Triple();
+            timeToLive = 1;
+            forever = true;
+        }
+     
+        public Triple getForceValue() {
+            if (timeToLive <= 0 && !forever) {
+                return new Triple();
+            }
+            return forceValue;
+        }
+     
+        public void decrementTimeToLive(double timeTick, RigidBody rb) {
+            if (!forever) {
+                timeToLive = timeToLive - timeTick;
+            }
+            if (timeToLive < 0 && !forever) {
+     
+                rb.setForcesChanged(true);
+            }
+        }
     }
 
-    public Triple getForceValue() {
-        if (timeToLive < 0 && !forever) {
-            return new Triple();
-        }
-        return forceValue;
-    }
-
-    public void decrementTimeToLive(double timeTick, RigidBody rb) {
-        if (!forever) {
-            timeToLive = timeToLive - timeTick;
-        }
-        if (timeToLive < 0 && !forever) {
-
-            rb.setForcesChanged(true);
-        }
-    }
-}
